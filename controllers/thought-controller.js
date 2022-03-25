7271b761ff239740a042919d5a384da8a9787629
Jacob <jacob.tobin.liberty@gmail.com>
2022-03-25 16:17:54 -0400

    thought controllers added

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,14 +2,60 @@ const { Thought } = require('../models');
 
 const thoughtController = {
   // GET all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+  },
 
   // GET single thought by _id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .select('-__v')
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
   // POST a new thought (add id to users thoughts aswell)
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.status(400).json(err));
+  },
 
   // PUT (update) an existing thought by _id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({message: 'No Thought found with this id'});
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
   // DELETE a thought by _id
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id'});
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
   // POST a reaction to a thought (add it to reactions array)
   // /api/thoughts/:thoughtId/reactions
